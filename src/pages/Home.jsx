68c5b63b2b3814f41a66d78bbb970fbe92ca5a2a
Alline <alline_franciely@hotmail.com>
2022-03-10 16:21:52 -0300

    requisito 3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
+import Image from '../images/shoppingcart.jpg';
+
 class Home extends React.Component {
   render() {
     return (
-      <h2 data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </h2>
+      <div>
+        <h2 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h2>
+        <input
+          type="text"
+          id="input-search"
+          placeholder="Pesquisar"
+        />
+        <Link data-testid="shopping-cart-button" to="/cart">
+          <img src={ Image } alt="carrinho de compras" />
+        </Link>
+      </div>
     );
   }
 }
