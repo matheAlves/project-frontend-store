6e4549d2796966040ae33b9cbd878429bdcdbe93
Aline Bury <alinebury@gmail.com>
2022-03-12 21:07:35 -0300

    Requisito 7

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class Product extends React.Component {
   render() {
-    const { product: { title, thumbnail, price } } = this.props; // vem das propriedades de product passada pelo .map do ProductList.
+    const { product: { title, thumbnail, price, id } } = this.props; // vem das propriedades de product passada pelo .map do ProductList.
     return (
       <div data-testid="product">
         <img src={ thumbnail } alt={ title } />
         <h3>{ title }</h3>
         <p>{ price }</p>
+        <Link
+          data-testid="product-detail-link"
+          to={ `/${id}` }
+        >
+          Saiba mais.
+        </Link>
       </div>
     );
   }
@@ -19,6 +26,7 @@ Product.propTypes = {
     title: PropTypes.string,
     thumbnail: PropTypes.string,
     price: PropTypes.number,
+    id: PropTypes.string,
   }).isRequired,
 };
 
---
+++ src/pages/Details.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { getProductDetails } from '../services/api';
+
+class Details extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      product: [],
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    this.setState(
+      { loading: true },
+      async () => {
+        const details = await getProductDetails(id);
+        this.setState({
+          product: details,
+          loading: false,
+        });
+      },
+    );
+  }
+
+  render() {
+    const { product, loading } = this.state;
+    return (
+      <div>
+        { loading ? 'Carregando...' : (
+          <div>
+            <img src={ product.thumbnail } alt={ product.title } />
+            <p data-testid="product-detail-name">{ product.title }</p>
+            <p>{ `R$ ${product.price}` }</p>
+            <p>{ product.warranty }</p>
+            <p>{ `Status do produto: ${product.status}` }</p>
+          </div>
+        ) }
+      </div>
+    );
+  }
+}
+
+Details.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
+
+export default Details;
