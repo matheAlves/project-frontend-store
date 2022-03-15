d6c1ca9c8843a791ccac9e32f80f30a5ad97ff34
Natã A.Rodrigues <nata26ar@gmail.com>
2022-03-15 19:34:54 -0300

    CSS da lista de produtos - correçoes a serem feitas apos o merge

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Image from '../images/shoppingcart.jpg';
 
 class Product extends React.Component {
   render() {
     const { product: { title, thumbnail, price, id } } = this.props; // vem das propriedades de product passada pelo .map do ProductList.
     const { addToCart } = this.props;
     return (
-      <div data-testid="product">
-        <img src={ thumbnail } alt={ title } />
+      <div className="product" data-testid="product">
+        <img src={ thumbnail } alt={ title } className="productImg" />
         <h3>{ title }</h3>
         <p>{ price }</p>
-        <Link
-          data-testid="product-detail-link"
-          to={ `/${id}` }
-        >
-          Saiba mais.
-        </Link>
-        <button
-          type="submit"
-          data-testid="product-add-to-cart"
-          onClick={ () => addToCart(thumbnail, title, price) }
-        >
-          Adicionar no carrinho
-        </button>
+        <div className="moreAdd">
+          <Link
+            data-testid="product-detail-link"
+            to={ `/${id}` }
+          >
+            Saiba mais.
+          </Link>
+          <button
+            type="submit"
+            data-testid="product-add-to-cart"
+            onClick={ () => addToCart(thumbnail, title, price) }
+          >
+            <img src={ Image } alt="Adicionar ao Carrinho" />
+          </button>
+        </div>
       </div>
     );
   }
