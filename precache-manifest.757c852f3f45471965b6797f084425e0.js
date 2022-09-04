131b25d84e98342a2f220667e83d8ce00a176fb2
Matheus S Alves <matheus.souza.alves@gmail.com>
2022-09-04 15:14:58 -0300

    Updates

---
+++ precache-manifest.757c852f3f45471965b6797f084425e0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd7853de6a23202c7cdeda58a748cc1e",
+    "url": "/project-frontend-store/index.html"
+  },
+  {
+    "revision": "b218520e004287473824",
+    "url": "/project-frontend-store/static/css/main.ef4f1dd4.chunk.css"
+  },
+  {
+    "revision": "0769ea30ebace40823df",
+    "url": "/project-frontend-store/static/js/2.6098f642.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/project-frontend-store/static/js/2.6098f642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b218520e004287473824",
+    "url": "/project-frontend-store/static/js/main.79371bf3.chunk.js"
+  },
+  {
+    "revision": "bd9db756dbc28da442a9",
+    "url": "/project-frontend-store/static/js/runtime-main.c2843ee3.js"
+  },
+  {
+    "revision": "7fb2da28bcd8fbef7f35bc7794a29b13",
+    "url": "/project-frontend-store/static/media/cart.7fb2da28.png"
+  },
+  {
+    "revision": "ccdc4f9a7450f063a3af01728dcda918",
+    "url": "/project-frontend-store/static/media/front.ccdc4f9a.gif"
+  },
+  {
+    "revision": "be5f03190e216d92976a613814dd8cec",
+    "url": "/project-frontend-store/static/media/shoppingcart.be5f0319.jpg"
+  },
+  {
+    "revision": "caacdb3470282a51e0bca29fab0d454e",
+    "url": "/project-frontend-store/static/media/trybee.caacdb34.png"
+  },
+  {
+    "revision": "1907807b295e25512ee5b8930a5ec875",
+    "url": "/project-frontend-store/static/media/voltar.1907807b.png"
+  }
+]);
